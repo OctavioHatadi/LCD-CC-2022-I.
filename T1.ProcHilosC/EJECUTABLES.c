06c044c602f687633e6d50eb9cd1eb8b9c71bc40
Octavio Hatadi <80992371+OctavioHatadi@users.noreply.github.com>
2021-09-23 21:40:15 -0500

    Add files via upload

---
+++ T1.ProcHilosC/EJECUTABLES.c
@@ -0,0 +1,41 @@
+//Ejecutables
+
+//enviomensajepipe.c
+gcc enviomensajepipe.c  -o enviomensajepipe
+./enviomensajepipe
+
+//EnvioMsjPipeFloat.c
+gcc EnvioMsjPipeFloat.c -o EnvioMsjPipeFloat
+./EnvioMsjPipeFloat
+
+//fork1.c
+gcc fork1.c -o fork1
+./fork1
+
+//fork2.c
+gcc fork2.c -o fork2
+./fork2
+
+//fork3.c
+gcc fork3.c -o fork1
+./fork3
+
+//identificacionprocesos.c
+gcc identificacionprocesos.c -o identificacionprocesos
+./identificacionprocesos
+
+//threads.c
+gcc threads.c -lpthread -o threads
+./threads
+
+//wait.c
+gcc wait.c -o wait
+./wait
+
+//waitoper.c
+gcc waitoper.c -o waitoper
+./waitoper
+
+//waitprocs.c
+gcc waitprocs.c -o waitprocs
+./waitprocs
